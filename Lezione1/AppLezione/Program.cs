393e8502b56d316511f67d205fbcb5eb52f3a975
Simon01 <zuga_simo@hotmail.it>
2018-09-15 11:49:04 +0200

    Esercizio 1

---
+++ Lezione1/AppLezione/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLezione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 55555;
+            Console.WriteLine($"il valore di a è {a}");
+            short s = (short)a;
+            Console.Write($"il valore di s è {s}");
+            Console.ReadLine();
+        }
+    }
+}
